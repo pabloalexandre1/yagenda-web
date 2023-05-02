a88fb5c1164b4224aa89f55ed0ac313851a79193
pabloalexandre1 <pablo.barros1@outlook.com>
2023-05-02 09:28:54 -0300

    Update PaymentTest.vue

--- src/views/PaymentTest.vue
+++ src/views/PaymentTest.vue
@@ -13,8 +13,8 @@ import axios from 'axios'
         name: 'PaymentTest',
         data() {
             return {
-                publish_key: 'pk_live_51LSSIPGHronIDZ9wa4sV02CTKE48uBcqZ8iPzs3rhZGT4tUi2ZPV4FfPiJunmgKyJr3evWv2DzJ1ZOUe21DGmB1F00hMD1BxBM',
-                secret_key: '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'
+                publish_key: '--STRIPE-KEY--',
+                secret_key: '--STRIPE-KEY--'
             }
         },
         methods: {
